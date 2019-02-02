2ffed5ea70a44fee6a116e096479ef6e816fc16c
visualstudio <visualstudio@122-PC29>
2019-02-01 21:27:46 -0500

    last commit

--- week4/Program.cs
+++ week4/Program.cs
@@ -1,61 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace C0727172_Week04
+﻿class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // ounntryside c = new countryside();
-            // c.run();
-        }
-    }
-    class LearningExperment
-    {
-        public void run()
-        {
-
-            Village Toronto;
-            Toronto = new Village();
-            Village a;
-            Toronto.villageName = "Version A";
-            a = Toronto;
-            Toronto = new Village();
-            Toronto.villageName = "Version B";
-            b = Toronto;
-            Toronto = new Village();
-            Toronto.villageName = "Version C";
-            c = Toronto;
-
-        }
-    }
-
-    class Village
-    {
-        public bool isAstrilde;
-        public Village nextVillage;
-        public Village prevVillage;
-        public string villageName;
-
-    }
-
-    class Countryside
-    {
-        Village Toronto, Maple, Ajax;
-
-        public void run()
-        {
-            Maple = new Village();
-            Toronto = new Village();
-            Ajax = new Village();
-            Maple.villageName = "Toronto";
-            Maple.nextVillage = Toronto;
-
-        }
+        Countryside c = new Countryside();
+        c.run();
+        c.Travel();
+        //new Test().forTest(); // Anonymous Object Reference
+        Console.ReadLine();
     }
 }
 
+class Village
+{
+    public bool isAstrilde = false;
+    public Village nextVillage;
+    public Village prevVillage;
+    public string villageName;
+}
+
+class Countryside
+{
+    Village Toronto, Ajax, Maple;
+
+    public Village CurrentVillage { get; private set; }
+
+    public void run()
+    {
+        Maple = new Village();
+        Maple.villageName = "Maple";
+        Toronto = new Village();
+        Toronto.villageName = "Toronto";
+        Ajax = new Village();
+        Ajax.isAstrilde = true;
+        Ajax.villageName = "Ajax";
+
+        Maple.nextVillage = Toronto;
+        Toronto.nextVillage = Ajax;
+        Ajax.nextVillage = null;
+    }
+
+    public void Travel()
+    {
+        CurrentVillage = Toronto;
+        while (true)
+        {
+            if (CurrentVillage.isAstrilde)
+                Console.WriteLine("Astrilde is in : " + CurrentVillage.villageName);
+            else
+                CurrentVillage = CurrentVillage.nextVillage;
+        }
+    }
+}
+}
